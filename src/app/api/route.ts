1b49c4921177a3f55d8bea14e2cc3f92f96ac2c2
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-06-14 17:22:35 +0200

    compose chains

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,28 @@
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { HumanChatMessage } from 'langchain/schema';
-
+import { OpenAI } from 'langchain/llms/openai';
+import { LLMChain, SimpleSequentialChain } from 'langchain/chains';
 import { NextResponse } from 'next/server';
+import { PromptTemplate } from 'langchain/prompts';
 
-const chat = new ChatOpenAI({
-  temperature: 0,
+const llm = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
+
+const promptTemplate = new PromptTemplate({
+  template: `You are a playwright. Given the title of play, it is your job to write a synopsis for that title.
+  Title: {title}
+  Playwright: This is a synopsis for the above play:`,
+  inputVariables: ['title'],
+});
+const synopsisChain = new LLMChain({ llm, prompt: promptTemplate });
+
+const reviewPromptTemplate = new PromptTemplate({
+  template: `You are a play critic from the New York Times. Given the synopsis of play, it is your job to write a review for that play.
+  Play Synopsis:
+  {synopsis}
+  Review from a New York Times play critic of the above play:`,
+  inputVariables: ['synopsis'],
+});
+const reviewChain = new LLMChain({ llm, prompt: reviewPromptTemplate });
 
 export async function GET(request: Request) {
   const prompt = new URL(request.url).searchParams.get('prompt');
@@ -15,12 +31,12 @@ export async function GET(request: Request) {
     return NextResponse.json({ message: 'Missing prompt' }, { status: 400 });
   }
 
-  const response = await chat
-    .call([new HumanChatMessage(prompt)])
-    .catch((error) => {
-      console.log(error);
-      return new Response(JSON.stringify({ error }));
-    });
+  const overallChain = new SimpleSequentialChain({
+    chains: [synopsisChain, reviewChain],
+  });
 
-  return NextResponse.json({ text: response.text });
+  const review = await overallChain.run(prompt);
+  console.log(review);
+
+  return NextResponse.json({ text: review });
 }
